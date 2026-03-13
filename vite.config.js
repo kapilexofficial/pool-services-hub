3ee4ed8d86da5164a289ec61ae6c7c204129564d
Lucas Desio <lucasdesio82@gmail.com>
2026-03-13 17:02:25 -0400

    nuclear: remove ALL vite dev files and references

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middlewareMode: true,
-    hmr: false, // Disable HMR in production
-  },
   build: {
     outDir: 'dist',
     minify: 'terser',
     sourcemap: false,
+  },
+  server: {
+    hmr: false,
   },
 })
